e99d45937cc2e36fd6f417e19ea87fa3c67917a8
Liliya Peravozchykava <lily.brightlight@mail.ru>
2023-01-24 21:59:52 +0300

    get requests in API + basic structire

---
+++ Epam.TestAutomation/Epam.TestAutomation.API/AppConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.TestAutomation.API
+{
+    public class AppConfiguration
+    {
+        public Uri BiblesBaseUrl { get; set; }
+
+        public Uri PhonesBaseUrl { get; set; }
+
+        public string ApiKey { get; set; }
+    }
+}
---
+++ Epam.TestAutomation/Epam.TestAutomation.API/BaseController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace Epam.TestAutomation.API
+{
+    public class BaseController
+    {
+        private readonly RestClient _restClient;
+
+        protected BaseController(CustomRestClient client, Service service, string apiKey = "")
+        {
+            _restClient = client.CreateRestClient(service);
+            if (service == Service.Bibles)
+            {
+                _restClient.AddDefaultHeader("api-key", apiKey);
+            }
+        }
+
+        protected (RestResponse Response, T Bibles) Get<T>(string resource)
+        {
+            var request = new RestRequest(resource, Method.Get);
+            var response = _restClient.ExecuteGet(request);
+
+            return (typeof(T) == typeof(RestResponse))
+                ? (response, default)
+                : (response, GetDeserializedView<T>(response));
+        }
+
+        private T? GetDeserializedView<T>(RestResponse response)
+        {
+            return JsonConvert.DeserializeObject<T>(response.Content);
+        }
+    }
+}
